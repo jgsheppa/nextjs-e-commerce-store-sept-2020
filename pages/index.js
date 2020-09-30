11da319ce43eaa9bd338efc6b45b5e65f235e8fc
James Sheppard <jgsheppard92@gmail.com>
2020-09-30 14:19:51 +0200

    First commit w next framework

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Book Nook</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main></main>
+    </>
+  );
+}
