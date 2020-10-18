3e5e6f44b7a267022152504ef622320ae3167b2a
James Sheppard <jgsheppard92@gmail.com>
2020-10-18 19:20:45 +0200

    Change import to require

--- util/database.js
+++ util/database.js
@@ -1,7 +1,7 @@
 import postgres from 'postgres';
 import dotenv from 'dotenv';
 import camelcaseKeys from 'camelcase-keys';
-import setPostgresDefaultsOnHeroku from './setPostgresDefaultsOnHeroku';
+const setPostgresDefaultsOnHeroku = require('./setPostgresDefaultsOnHeroku');
 
 setPostgresDefaultsOnHeroku();
 
